361a15dd39e69057692dfc589d63e436158af186
ADWFC-Testmodi <jeryspam@web.de>
2025-03-15 11:03:23 +0100

    Change script for darkmode

--- script.js
+++ script.js
@@ -80,3 +80,13 @@ function hideCookieBanner() {
         localStorage.setItem('cookieBannerClosed', 'true');
     }, 1000);
 }
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+}
+
+// Dark Mode beim Laden prüfen
+//if (localStorage.getItem("darkMode") === "true") {
+  //  document.body.classList.add("dark-mode");
+//}
